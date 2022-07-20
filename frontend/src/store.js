705d547d209bfc8531b418a8579dbe7ec6fa11ab
Erik Truckner <75428920+ABSURD0@users.noreply.github.com>
2022-07-20 14:59:52 -0400

    FRONTEND COMMIT
    FRONTEND COMMIT

---
+++ frontend/src/store.js
@@ -0,0 +1,15 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import thunk from 'redux-thunk' // I still add this for my reference so I know thunk middleware is added
+import { productListReducer } from './reducers/productReducers'
+
+const reducer = combineReducers({
+  productList: productListReducer,
+})
+
+const store = configureStore({
+  reducer: {},
+  preloadedState: {},
+  middleware: [thunk],
+})
+
+export default store
